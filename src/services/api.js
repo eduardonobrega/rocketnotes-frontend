d888dfcf0d22932ae40cff7658430cc4360657bf
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-02-23 19:30:33 -0300

    baseURL api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://rocketnotes-api-1t0q.onrender.com',
+  baseURL: 'https://rocketnotes-api-sz60.onrender.com',
 });
